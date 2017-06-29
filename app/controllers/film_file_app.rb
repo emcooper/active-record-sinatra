38201be893053a157c93d2d32d6903a3cf3be5e7
Ellen Cooper <ellencooper74@gmail.com>
2017-06-28 18:40:32 -0600

    completes tutorial

--- app/controllers/film_file_app.rb
+++ app/controllers/film_file_app.rb
@@ -1,5 +1,11 @@
 class FilmFile < Sinatra::Base
+  get '/genres' do
+    @genres = Genre.all
+    erb :genre_index
+  end
+
   get '/films' do
+    @films = Film.all
     erb :films_index
   end
 end
